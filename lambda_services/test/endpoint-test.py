aa3078b8678e02036b99ab39cc70221269fef6b9
admin <admin>
2018-11-30 19:59:41 -0500

    Updated Lambda service config

--- lambda_services/test/endpoint-test.py
+++ lambda_services/test/endpoint-test.py
@@ -4,7 +4,7 @@ from dataiku.apinode.predict.predictor import ClassificationPredictor
 import pandas as pd
  
 import os
-import cPickle as pickle
+import pickle
 from sklearn.externals import joblib
  
 class MyPredictor(ClassificationPredictor):
